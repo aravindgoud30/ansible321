3cd1219bf2f1606d6ea2cee8f7eb906ee5339b5f
aravind goud <aravindgoud30@gmail.com>
2023-05-11 13:58:22 +0530

    changes done

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "sampleinstance" {
 resource "aws_security_group" "samplegroup" {
   name        = "samplegroup"
   description = "allow sample traffic"
-
+ }
   ingress {
     description      = "TLS from VPC"
     from_port        = 0
@@ -28,7 +28,6 @@ resource "aws_security_group" "samplegroup" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    ipv6_cidr_blocks = []
     prefix_list_ids  = []
     self             = false
   }
