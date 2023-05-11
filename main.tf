d3311a46cbe773467c791de72eecfcede935909d
aravind goud <aravindgoud30@gmail.com>
2023-05-11 14:52:15 +0530

    change8

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-provider "aws" {
-  region       = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "sampleinstance" {
   ami                     = "ami-0dcc1e21636832c5d"
   instance_type           = "t2.micro"
